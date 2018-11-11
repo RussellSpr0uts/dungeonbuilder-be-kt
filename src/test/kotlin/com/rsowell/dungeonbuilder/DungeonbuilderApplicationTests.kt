e1e353eace9c3a5f04e2b5d086d0ae6e440ec486
RussellSpr0uts <russellsowell88@gmail.com>
2018-11-11 08:51:20 -0500

    Initial commit

---
+++ src/test/kotlin/com/rsowell/dungeonbuilder/DungeonbuilderApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.rsowell.dungeonbuilder
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DungeonbuilderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
